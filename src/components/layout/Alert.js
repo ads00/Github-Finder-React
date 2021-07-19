bd1722b33284089b70b9152166c391fe6162cb42
ads00 <adshayanbalachandran@gmail.com>
2021-07-19 16:12:18 +0530

    App completed with context API implemented

--- src/components/layout/Alert.js
+++ src/components/layout/Alert.js
@@ -1,10 +1,15 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import AlertContext from '../../context/alert/alertContext';
 
-const Alert = ({ alert: { msg, type } }) => {
+const Alert = () => {
+    const alertContext = useContext(AlertContext);
+
+    const alert = alertContext.alert;
+
     return (
         alert !== null && (
-            <div className={`alert alert-${type}`}>
-                <i className="fas fa-info-circle"></i> {msg}
+            <div className={`alert alert-${alert.type}`}>
+                <i className="fas fa-info-circle"></i> {alert.msg}
             </div>
         )
         
--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,10 +1,14 @@
-import React, { Fragment, useEffect } from 'react';
+import React, { Fragment, useEffect, useContext } from 'react';
 import Spinner from '../layout/Spinner';
 import Repos from '../repos/Repos';
-import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import GithubContext from '../../context/github/githubContext';
 
-const User = ({ loading, user, repos, getUser, getUserRepos, match }) => {
+const User = ({ match }) => {
+    const githubContext = useContext(GithubContext);
+
+    const { loading, user, getUser, getUserRepos } = githubContext;
+
     useEffect(() => {
         getUser(match.params.login);
         getUserRepos(match.params.login);
@@ -83,18 +87,10 @@ const User = ({ loading, user, repos, getUser, getUserRepos, match }) => {
                 </div>
             </div>
 
-            <Repos repos={repos} />
+            <Repos />
         </Fragment>
     )
     
-}
-
-User.defaultProps = {
-    loading: PropTypes.bool,
-    user: PropTypes.object.isRequired,
-    repos: PropTypes.array.isRequired,
-    getUser: PropTypes.func.isRequired,
-    getUserRepos: PropTypes.func.isRequired,
 }
 
 export default User
